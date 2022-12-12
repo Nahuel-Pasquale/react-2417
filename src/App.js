2729f87b8c70dfcc67db46a73f6be4b097322e07
npasquale <nahuel.pasquale@medicus.com.ar>
2022-12-12 18:05:06 -0300

    clase - 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import Formulario from './components/clase-04/Formulario';
+import Logo from './components/logo/Logo';
+import GlobalStyle from './globalStyles/GlobalStyles';
+// import Formulario from './components/clase-04/Formulario';
 // import Lista from './components/clase-02/Lista';
 // import Title from './components/clase-02/Title';
 
 function App() {
   return (
     <>
-      <Formulario />
+      <Logo />
+      <GlobalStyle />
     </>
   );
 }
